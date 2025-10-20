bd1bcd68bc506ce40465f0dee3e099e56b254b10
cajomech-arch <cajomech@gmail.com>
2025-10-20 17:04:17 +0100

    Implement stationarity tests and ARIMA modeling
    Added ADF test for stationarity and ARIMA model fitting.

--- project_temp.R
+++ project_temp.R
@@ -1,6 +1,7 @@
 library(fpp3)
 library(forecast)
 library(tseries)
+library(urca)
 
 #Convert time column to Date format and filter from 1990 onward
 US_City_Temp_Data$time <- as.Date(US_City_Temp_Data$time)
@@ -20,3 +21,26 @@ plot(ny_ts,
      main = "Monthly Average Temperature - New York",
      xlab = "Year", 
      ylab = "Temperature")
+
+#stationarity: check with a ADF test for stationarity
+
+adf.test(ny_ts, k = 12) #K is 1 year (12 months) from now
+
+# if it is not stationary, let's perform a first diff.
+
+ny_ts_d1 <- diff(ny_ts, differences = 1)
+adf.test(ny_ts_d1, k=12)
+
+autoplot(ny_ts_d1)
+
+#ACF and PACF functions
+
+pacf(ny_ts_d1) #AR order p = 1
+acf(ny_ts_d1) #MA order q = 1
+
+tsMod <- Arima(y = ny_ts, order = c(1,1,1)) #tsMod is the ARIMA(1,1,1) model
+
+print(tsMod)
+
+forecast(tsMod, h = 12)
+autoplot(forecast(tsMod, h = 12))
